d53d7fe371714ff79cacb0f5b2d3e4243a9f2617
Alanoud <alanood94895@gmail.com>
2026-04-22 18:42:50 +0400

    TASk3:equalIsNot

--- src/equalIsNot.java
+++ src/equalIsNot.java
@@ -10,6 +10,7 @@ public class equalIsNot {
         int notCount = 0;
         for (int i = 0; i < str.length(); i++) {
             if (i <= str.length() - 2 && str.substring(i, i + 2).equals("is")) {
+                isCount++;
 
 
     }
