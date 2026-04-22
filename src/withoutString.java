d8c96aebc3aabc1a35e51f54814c8939f12ca777
Alanoud <alanood94895@gmail.com>
2026-04-22 18:37:58 +0400

    TASk2:withoutString

--- src/withoutString.java
+++ src/withoutString.java
@@ -1,11 +1,15 @@
 public class withoutString {
     public static void main(String[] args) {
-        String base="Hello there";
+        String base = "Hello there";
         String remove = "llo";
 
-        for (int i =0; i< base.length(); i++){
-            if (base.contains(remove)){
-                base =base.replace(remove, "");
+        for (int i = 0; i < base.length(); i++) {
+            if (base.contains(remove)) {
+                base = base.replace(remove, "");
 
             }
+        }
+        System.out.println(base);
+
+    }
 }
