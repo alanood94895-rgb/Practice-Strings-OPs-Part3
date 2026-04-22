2c12ee2ffc2f270c78c2f2a77894c31b1481a55a
Alanoud <alanood94895@gmail.com>
2026-04-22 18:48:26 +0400

    TASk4:gHappy

--- src/gHappy.java
+++ src/gHappy.java
@@ -7,6 +7,13 @@ public class gHappy {
         for(int i = 0; i < str.length(); i++){
             if(str.charAt(i) == 'g'){
                 if( (i > 0 && str.charAt(i-1) == 'g') ||
+                        (i < str.length()-1 && str.charAt(i+1) == 'g') ){
+                    found = true;
+                } else {
+                    found = false;
+                }
+            }
+        }
         }
     }
 }
