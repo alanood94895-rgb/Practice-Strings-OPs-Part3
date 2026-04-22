d902615057f8e7456372ddcf070b2e311c11685b
Alanoud <alanood94895@gmail.com>
2026-04-22 18:36:59 +0400

    TASk2:withoutString

--- src/withoutString.java
+++ src/withoutString.java
@@ -2,4 +2,6 @@ public class withoutString {
     public static void main(String[] args) {
         String base="Hello there";
         String remove = "llo";
+
+        for (int i =0; i< base.length(); i++){
 }
