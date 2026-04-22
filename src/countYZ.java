d034b0f069342db429dcf4a17a38aa1acb2b562d
Alanoud <alanood94895@gmail.com>
2026-04-22 18:29:56 +0400

    TASK1:countYZ

--- src/countYZ.java
+++ src/countYZ.java
@@ -1,6 +1,8 @@
 public class countYZ {
     public static void main(String[] args) {
         System.out.println(countYZ ("fez day"));
+    }
+    public static Integer countYZ (String str){
 
     }
 
