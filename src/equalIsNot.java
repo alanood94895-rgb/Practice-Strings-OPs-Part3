41ed457ec577af51a983dcf395b66c7834305333
Alanoud <alanood94895@gmail.com>
2026-04-22 18:40:39 +0400

    TASk3:equalIsNot

--- src/equalIsNot.java
+++ src/equalIsNot.java
@@ -2,5 +2,8 @@ public class equalIsNot {
     public static void main(String[] args) {
         System.out.println(equalIsNot("This is not"));        // false
         System.out.println(equalIsNot("This is notnot"));// true
+        System.out.println(equalIsNot("noisxxnotyynotxisi")); // true
+
     }
+
 }
