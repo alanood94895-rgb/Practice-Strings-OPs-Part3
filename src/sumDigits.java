5df6c96d2ef29087a61cdc72a633c9467a25da4d
Alanoud <alanood94895@gmail.com>
2026-04-22 21:00:43 +0400

    TASk6:sumDigits

--- src/sumDigits.java
+++ src/sumDigits.java
@@ -1,6 +1,6 @@
 public class sumDigits {
     public static void main (String[] args){
-        System.out.println(sumDigits ("aa1bc2d3"));
+        System.out.println(sumDigits ("aa1bc2d3")); //6
         System.out.println(sumDigits ("aa11b33"));
     }
     public static int sumDigits(String str) {
