cc43db217966a19319c7e170c30cc4ccdfc2dcb9
Alanoud <alanood94895@gmail.com>
2026-04-22 18:31:42 +0400

    TASK1:countYZ

--- src/countYZ.java
+++ src/countYZ.java
@@ -7,6 +7,12 @@ public class countYZ {
         str = str.toLowerCase();
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-    }
+            if (c == 'y' || c == 'z') {
+                if (i == str.length() - 1 || !Character.isLetter(str.charAt(i + 1))) {
+                    count++;
+                }
+            }
+        }
+
 
 }
