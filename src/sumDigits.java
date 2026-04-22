9019c4751ab74ba0758e4ba524fd9e48ab73ec49
Alanoud <alanood94895@gmail.com>
2026-04-22 20:58:40 +0400

    TASk6:sumDigits

--- src/sumDigits.java
+++ src/sumDigits.java
@@ -6,6 +6,7 @@ public class sumDigits {
     public static int sumDigits(String str) {
         int sum = 0;
         for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
 
 
 }
