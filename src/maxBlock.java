02802778bcfe4f7afe1a2d7db0a879c89addded8
Alanoud <alanood94895@gmail.com>
2026-04-23 09:56:31 +0400

    TASk9:maxBlock

--- src/maxBlock.java
+++ src/maxBlock.java
@@ -1,8 +1,9 @@
 public class maxBlock {
-public static void main (String[] args){
-    System.out.println();
-    System.out.println();
-}
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+    }
+
     public static int maxBlock(String str) {
         int max = 0;
         int count = 0;
@@ -18,4 +19,5 @@ public static void main (String[] args){
         }
         return max;
     }
+}
 
