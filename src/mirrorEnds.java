521ead575819cc224fa08630cf4bc5ef1c91b4f5
Alanoud <alanood94895@gmail.com>
2026-04-23 09:47:30 +0400

    TASk8:mirrorEnds

--- src/mirrorEnds.java
+++ src/mirrorEnds.java
@@ -2,6 +2,7 @@ public class mirrorEnds {
 public static void main (String[] args){
     System.out.println(mirrorEnds);
     System.out.println(mirrorEnds);
-
 }
+    public static String mirrorEnds(String str) {
+
 }
