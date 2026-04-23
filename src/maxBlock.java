dcbffe80dcec41fde5e1d05070076b7ffa18b469
Alanoud <alanood94895@gmail.com>
2026-04-23 09:54:51 +0400

    TASk9:maxBlock

--- src/maxBlock.java
+++ src/maxBlock.java
@@ -7,5 +7,6 @@ public static void main (String[] args){
         int max = 0;
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
+            if (i == 0 || str.charAt(i) != str.charAt(i - 1)) {
 
 }
