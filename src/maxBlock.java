0dcb1920e3236f0bc5c6b5dafce9fe4064f8910d
Alanoud <alanood94895@gmail.com>
2026-04-23 09:56:12 +0400

    TASk9:maxBlock

--- src/maxBlock.java
+++ src/maxBlock.java
@@ -11,9 +11,11 @@ public static void main (String[] args){
                 count = 1;
             } else {
                 count++;
-}
+            }
             if (count > max) {
                 max = count;
-
             }
+        }
+        return max;
+    }
 
