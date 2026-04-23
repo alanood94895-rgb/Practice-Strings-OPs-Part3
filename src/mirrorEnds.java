21cbfd6dfd58ef142c885e2a261ae472d460a1f9
Alanoud <alanood94895@gmail.com>
2026-04-23 09:48:18 +0400

    TASk8:mirrorEnds

--- src/mirrorEnds.java
+++ src/mirrorEnds.java
@@ -6,4 +6,6 @@ public static void main (String[] args){
     public static String mirrorEnds(String str) {
         String result = "";
         for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+        }
 }
