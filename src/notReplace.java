06406582f602c9966df4c9fdf11b2d1e06bb1abf
Alanoud <alanood94895@gmail.com>
2026-04-23 10:18:07 +0400

    TASk11:notReplace

--- src/notReplace.java
+++ src/notReplace.java
@@ -3,4 +3,26 @@ public static void main(String[] args){
     System.out.println();
     System.out.println();
 }
+    public static String notReplace(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            // check if "is" موجودة
+            if (i + 1 < str.length() && str.substring(i, i + 2).equals("is")) {
+
+                boolean before = (i == 0 || !Character.isLetter(str.charAt(i - 1)));
+                boolean after = (i + 2 >= str.length() || !Character.isLetter(str.charAt(i + 2)));
+
+                if (before && after) {
+                    result += "is not";
+                    i++; // skip next 's'
+                    continue;
+                }
+            }
+
+            result += str.charAt(i);
+        }
+
+        return result;
+    }
 }
