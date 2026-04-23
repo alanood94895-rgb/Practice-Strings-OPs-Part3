82521c5b39609bedb0763dfa756a0dbd7d3b950e
Alanoud <alanood94895@gmail.com>
2026-04-23 10:18:35 +0400

    TASk11:notReplace

--- src/notReplace.java
+++ src/notReplace.java
@@ -7,7 +7,7 @@ public static void main(String[] args){
         String result = "";
 
         for (int i = 0; i < str.length(); i++) {
-            // check if "is" موجودة
+
             if (i + 1 < str.length() && str.substring(i, i + 2).equals("is")) {
 
                 boolean before = (i == 0 || !Character.isLetter(str.charAt(i - 1)));
