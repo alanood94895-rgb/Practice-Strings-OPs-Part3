0476f1f34c0626e252d8d90f2d0e821841475a90
Alanoud <alanood94895@gmail.com>
2026-04-23 09:43:35 +0400

    TASk7:sameEnds

--- src/sameEnds.java
+++ src/sameEnds.java
@@ -1,20 +1,15 @@
 public class sameEnds {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("abXYab"); //"ab"
         System.out.println("xx"); //"x"
     }
+
     public static String sameEnds(String str) {
-        String result = "";
-        for (int i = 0; i < str.length() / 2; i++) {
+        for (int i = str.length() / 2; i > 0; i--) {
             if (str.substring(0, i).equals(str.substring(str.length() - i))) {
-                result += str.substring(0, i);
-                for (int i = 0; i <= str.length() / 2; i++) {
-                    if (str.substring(0, i).equals(str.substring(str.length() - i))) {
-                        result = str.substring(0, i);
-                        ;
-                    }
-                }
-                return result;
+                return str.substring(0, i);
             }
         }
+        return "";
     }
+}
