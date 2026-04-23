98268492553632465640f4b86976159befbcbcdf
Alanoud <alanood94895@gmail.com>
2026-04-23 09:39:54 +0400

    TASk7:sameEnds

--- src/sameEnds.java
+++ src/sameEnds.java
@@ -6,4 +6,11 @@ public class sameEnds {
     public static String sameEnds(String str) {
         String result = "";
         for(int i=0; i<str.length()/2;i++){
+            if(str.substring(0,i).equals( str.substring(str.length()-i))){
+                result += str.substring(0,i);
+                for(int i = 0; i <= str.length() / 2; i++) {
+                    if(str.substring(0, i).equals(str.substring(str.length() - i))) {
+                        result = str.substring(0, i);;
+
+        }
 }
