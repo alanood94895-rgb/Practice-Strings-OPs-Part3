8bb4bf86857771169b6e9332abf8aa4f5dfd812b
Alanoud <alanood94895@gmail.com>
2026-04-23 09:48:38 +0400

    TASk8:mirrorEnds

--- src/mirrorEnds.java
+++ src/mirrorEnds.java
@@ -7,5 +7,10 @@ public static void main (String[] args){
         String result = "";
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+                result += str.charAt(i);
+            } else {
+
+            }
+            }
         }
 }
